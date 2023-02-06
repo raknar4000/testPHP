40f921bee4e591dce14f7c62f4cbd8ce6c1d3604
Ragnar Lall <ragnarlall@gmail.com>
2023-02-07 01:03:49 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php 
+
+
+
+?>
+
+<!DOCTYPE html>
+<html> 
+<head>
+    
+</head>
+<body>
+
+
+<?php include('templates/header.php')?>
+<?php include('templates/footer.php')?>
+    
+   
+</body>
+</html>
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+
+    <footer class="section">
+        <div class="center grey text">&copy; Copyright 2023 Ragnars Pizza</div>
+    </footer>
+
+</body>
