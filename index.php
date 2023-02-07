6bc10ef9da12a68b3f6ff549fb993436a4a1d4a2
Ragnar Lall <ragnarlall@gmail.com>
2023-02-07 02:22:02 +0200

    conn

--- index.php
+++ index.php
@@ -1,6 +1,34 @@
 <?php 
 
+$name = "localhost";
+$uname = "root";
+$password = "";
+$db_name = "ragnarspizza";
 
+$conn = mysqli_connect($name, $uname, $password, $db_name);
+
+// check the connection
+
+if(!$conn) {
+    echo "Connection Failed";
+}
+
+	// write query for all pizzas
+	$sql = 'SELECT title, ingredients, id FROM pizzas ORDER BY created_at';
+
+	// get the result set (set of rows)
+	$result = mysqli_query($conn, $sql);
+
+	// fetch the resulting rows as an array
+	$pizzas = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+	// free the $result from memory (good practise)
+	mysqli_free_result($result);
+
+	// close connection
+	mysqli_close($conn);
+
+	print_r($pizzas);
 
 ?>
 
